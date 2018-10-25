1d952724707e3d8ea0e1358999f449ca8b4f5571
Chris Hulton <chris@codeclimate.com>
2018-10-25 11:43:05 -0400

    Output learned line, predictions vs. actual and weights (#4)
    This is still using just one feature (PR size) and attempting to predict
    the cycle time (now converted from days to more granular minutes).
    
    Next will work on incorporating additional features, which will remove
    the simple learned line visualization but should make the predictions
    vs. actual graph more accurate.
    
    Co-authored-by: Scott Larkin scott@codeclimate.com

--- feature-or-not.py
+++ feature-or-not.py
@@ -19,16 +19,19 @@ pull_requests_dataframe = pd.read_csv("/pull_requests.csv")
 
 pull_requests_dataframe = pull_requests_dataframe.reindex(
         np.random.permutation(pull_requests_dataframe.index))
-pull_requests_dataframe
+
+# Filter out rows where relevant features are null
+pull_requests_dataframe = pull_requests_dataframe.dropna(subset=['size', 'cycle_time_in_days'])
 
 """
 To get started with ML, first predict one label from one feature
 We will predict time-to-review based on the size of the PR
+
+See: https://developers.google.com/machine-learning/crash-course/first-steps-with-tensorflow/programming-exercises
 """
 def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
     """Trains a linear regression model of one feature.
-    See: https://developers.google.com/machine-learning/crash-course/first-steps-with-tensorflow/programming-exercises
-
+  
     Args:
       features: pandas DataFrame of features
       targets: pandas DataFrame of targets
@@ -38,7 +41,7 @@ def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
     Returns:
       Tuple of (features, labels) for next data batch
     """
-  
+
     # Convert pandas data into a dict of np arrays.
     features = {key:np.array(value) for key,value in dict(features).items()}                                           
  
@@ -54,78 +57,125 @@ def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
     features, labels = ds.make_one_shot_iterator().get_next()
     return features, labels
 
-# Filter out rows where time to review is null
-filtered_pull_requests_dataframe = pull_requests_dataframe[pull_requests_dataframe['time_to_review_in_minutes'].notnull()]
+def train_model(learning_rate, steps, batch_size, input_feature):
+  """Trains a linear regression model.
+  
+  Args:
+    learning_rate: A `float`, the learning rate.
+    steps: A non-zero `int`, the total number of training steps. A training step
+      consists of a forward and backward pass using a single batch.
+    batch_size: A non-zero `int`, the batch size.
+    input_feature: A `string` specifying a column from `pull_requests_dataframe`
+      to use as input feature.
+      
+  Returns:
+    A Pandas `DataFrame` containing targets and the corresponding predictions done
+    after training the model.
+  """
+  
+  periods = 10
+  steps_per_period = steps / periods
 
-# Define the input feature: size (additions + deletions). 
-my_feature = filtered_pull_requests_dataframe[["size"]]
+  my_feature = input_feature
+  my_feature_data = pull_requests_dataframe[[my_feature]].astype('float32')
+  my_label = "cycle_time_in_minutes"
+  targets = pull_requests_dataframe[my_label].astype('float32')
 
-# Configure a numeric feature column for time_to_review_in_minutes.
-feature_columns = [tf.feature_column.numeric_column("size")]
+  # Create input functions.
+  training_input_fn = lambda: my_input_fn(my_feature_data, targets, batch_size=batch_size)
+  predict_training_input_fn = lambda: my_input_fn(my_feature_data, targets, num_epochs=1, shuffle=False)
+  
+  # Create feature columns.
+  feature_columns = [tf.feature_column.numeric_column(my_feature)]
+    
+  # Create a linear regressor object.
+  my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
+  my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+  linear_regressor = tf.estimator.LinearRegressor(
+      feature_columns=feature_columns,
+      optimizer=my_optimizer
+  )
 
-# Define the label.
-targets = filtered_pull_requests_dataframe["time_to_review_in_minutes"]
+  # Set up to plot the state model's line.
+  plt.figure(figsize=(15, 6))
+  plt.subplot(1, 2, 1)
+  plt.title("Learned Line")
+  plt.ylabel(my_label)
+  plt.xlabel(my_feature)
+  sample = pull_requests_dataframe.sample(n=500)
+  plt.scatter(sample[my_feature], sample[my_label])
 
-# Use gradient descent as the optimizer for training the model.
-my_optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.00002)
-my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
+  # Train the model, but do so inside a loop so that we can periodically assess
+  # loss metrics.
+  print("Training model...")
+  print("RMSE (on training data):")
+  root_mean_squared_errors = []
+  for period in range (0, periods):
+    # Train the model, starting from the prior state.
+    linear_regressor.train(
+        input_fn=training_input_fn,
+        steps=steps_per_period,
+    )
+    # Take a break and compute predictions.
+    predictions = linear_regressor.predict(input_fn=predict_training_input_fn)
+    predictions = np.array([item['predictions'][0] for item in predictions])
+    
+    # Compute loss.
+    root_mean_squared_error = math.sqrt(
+      metrics.mean_squared_error(predictions, targets))
+    # Occasionally print the current loss.
+    print("  period %02d : %0.2f" % (period, root_mean_squared_error))
+    # Add the loss metrics from this period to our list.
+    root_mean_squared_errors.append(root_mean_squared_error)
 
-# Configure the linear regression model with our feature columns and optimizer.
-# Set a learning rate of 0.0000001 for Gradient Descent.
-linear_regressor = tf.estimator.LinearRegressor(
-    feature_columns=feature_columns,
-    optimizer=my_optimizer
-)
+  print("Model training finished.")
 
-_ = linear_regressor.train(
-    input_fn = lambda:my_input_fn(my_feature, targets, batch_size=5),
-    steps=500
-)
+  # Plot sampled data vs. prediction line
+  y_extents = np.array([0, sample[my_label].max()])
 
-# Create an input function for predictions.
-# Note: Since we're making just one prediction for each example, we don't 
-# need to repeat or shuffle the data here.
-prediction_input_fn =lambda: my_input_fn(my_feature, targets, num_epochs=1, shuffle=False)
+  weight = linear_regressor.get_variable_value('linear/linear_model/%s/weights' % input_feature)[0]
+  bias = linear_regressor.get_variable_value('linear/linear_model/bias_weights')
 
-# Call predict() on the linear_regressor to make predictions.
-predictions = linear_regressor.predict(input_fn=prediction_input_fn)
+  x_extents = (y_extents - bias) / weight
+  x_extents = np.maximum(np.minimum(x_extents,
+    sample[my_feature].max()),
+    sample[my_feature].min())
+  y_extents = weight * x_extents + bias
+  plt.plot(x_extents, y_extents) 
 
-# Format predictions as a NumPy array, so we can calculate error metrics.
-predictions = np.array([item['predictions'][0] for item in predictions])
+  # Create a table with calibration data.
+  calibration_data = pd.DataFrame()
+  calibration_data["predictions"] = pd.Series(predictions)
+  calibration_data["targets"] = pd.Series(targets)
+  print(calibration_data.describe())
 
-# Print Mean Squared Error and Root Mean Squared Error.
-mean_squared_error = metrics.mean_squared_error(predictions, targets)
-root_mean_squared_error = math.sqrt(mean_squared_error)
-print("Mean Squared Error (on training data): %0.3f" % mean_squared_error)
-print("Root Mean Squared Error (on training data): %0.3f" % root_mean_squared_error)
+  print("Final RMSE (on training data): %0.2f" % root_mean_squared_error)
+  print([(v, linear_regressor.get_variable_value(v)) for v in linear_regressor.get_variable_names()])
 
-calibration_data = pd.DataFrame()
-calibration_data["predictions"] = pd.Series(predictions)
-calibration_data["targets"] = pd.Series(targets)
-print(calibration_data.describe())
+  # Plot predicted vs. actual
+  plt.subplot(1, 2, 2)
+  plt.title("Predictions vs. Actual")
+  plt.xlabel("predicted_cycle_time_in_minutes")
+  plt.ylabel("actual_cycle_time_in_minutes")
+  plt.scatter(calibration_data["predictions"], calibration_data["targets"])
+  plt.savefig("/tmp/fig.png")
 
-sample = filtered_pull_requests_dataframe.sample(n=500)
-# Get the min and max total_rooms values.
-x_0 = sample["size"].min()
-x_1 = sample["size"].max()
+  # Output model bias + weights
+  print(linear_regressor.model_fn)
 
-# Retrieve the final weight and bias generated during training.
-weight = linear_regressor.get_variable_value('linear/linear_model/size/weights')[0]
-bias = linear_regressor.get_variable_value('linear/linear_model/bias_weights')
+  return calibration_data
 
-# Get the predicted median_house_values for the min and max total_rooms values.
-y_0 = weight * x_0 + bias 
-y_1 = weight * x_1 + bias
+pull_requests_dataframe["size"] = (
+    pull_requests_dataframe["size"]).apply(lambda x: min(x, 10000))
 
-# Plot our regression line from (x_0, y_0) to (x_1, y_1).
-plt.plot([x_0, x_1], [y_0, y_1], c='r')
+pull_requests_dataframe["cycle_time_in_days"] = (
+    pull_requests_dataframe["cycle_time_in_days"]).apply(lambda x: min(x, 20))
 
-# Label the graph axes.
-plt.ylabel("time_to_review_in_minutes")
-plt.xlabel("size")
+pull_requests_dataframe["cycle_time_in_minutes"] = (
+    pull_requests_dataframe["cycle_time_in_days"]).apply(lambda x: x * 24 * 60)
 
-# Plot a scatter plot from our data sample.
-plt.scatter(sample["size"], sample["time_to_review_in_minutes"])
-
-# Save graph.
-plt.savefig("/tmp/fig.png")
+calibration_data = train_model(
+    learning_rate=0.00001,
+    steps=5000,
+    batch_size=200,
+    input_feature="size")
