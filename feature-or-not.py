a10cb19aa0ac87b41dacd80a83bb6e96a98075b6
Chris Hulton <chris@codeclimate.com>
2018-10-26 13:38:23 -0400

    Predict feature or not using LinearClassifier (#7)
    This uses 3 GH features (additions, deletions, changed file count) and 2
    velocity features (time to review, review cycles) to try to determine
    whether a PR is a feature or not.
    
    I wouldn't expect this to be a very good model, and it isn't - but, it is
    better than random chance!
    
    ```
    AUC on the validation set: 0.64
    Accuracy on the validation set: 0.57
    ```

--- feature-or-not.py
+++ feature-or-not.py
@@ -21,10 +21,7 @@ pull_requests_dataframe = pd.read_csv("/pull_requests.csv")
 pull_requests_dataframe = pull_requests_dataframe.reindex(
         np.random.permutation(pull_requests_dataframe.index))
 
-pull_requests_dataframe["body_char_count"] = (
-    pull_requests_dataframe["body"].str.len())
-
-relevant_features = ['additions_count', 'deletions_count', 'changed_files_count', 'body_char_count', 'time_to_review_in_minutes']
+relevant_features = ['additions_count', 'deletions_count', 'changed_files_count', 'review_cycles_count', 'time_to_review_in_minutes']
 
 def linear_scale(series):
   min_val = series.min()
@@ -46,14 +43,14 @@ def normalize(examples_dataframe):
 
   processed_features['additions_count'] = log_normalize(examples_dataframe['additions_count'])
   processed_features['deletions_count'] = log_normalize(examples_dataframe['deletions_count'])
-  processed_features['changed_files_count'] = linear_scale(examples_dataframe['changed_files_count'])
-  processed_features['body_char_count'] = linear_scale(examples_dataframe['body_char_count'])
+  processed_features['changed_files_count'] = log_normalize(examples_dataframe['changed_files_count'])
+  processed_features['review_cycles_count'] = examples_dataframe['review_cycles_count']
   processed_features['time_to_review_in_minutes'] = log_normalize(examples_dataframe['time_to_review_in_minutes'])
 
   return processed_features
 
 # Filter out rows where relevant features are null
-pull_requests_dataframe = pull_requests_dataframe.dropna(subset=(['cycle_time_in_days'] + relevant_features))
+pull_requests_dataframe = pull_requests_dataframe.dropna(subset=(['categorized_as_feature_by_human'] + relevant_features))
 
 # Trim outliers
 pull_requests_dataframe["additions_count"] = (
@@ -67,10 +64,6 @@ pull_requests_dataframe["changed_files_count"] = (
 
 pull_requests_dataframe["time_to_review_in_minutes"] = (
     pull_requests_dataframe["time_to_review_in_minutes"].apply(lambda x: min(x, 1000)))
-
-# There is a huge amount of variability here - cap at 5 days
-pull_requests_dataframe["cycle_time_in_days"] = (
-    pull_requests_dataframe["cycle_time_in_days"].apply(lambda x: min(x, 5)))
 
 print(pull_requests_dataframe.describe())
 
@@ -108,8 +101,8 @@ def preprocess_targets(pull_requests_dataframe):
   """
   output_targets = pd.DataFrame()
   # Scale the target to be in minutes
-  output_targets["cycle_time_in_minutes"] = (
-      pull_requests_dataframe["cycle_time_in_days"] * 24 * 60)
+  output_targets["categorized_as_feature_by_human"] = (
+      pull_requests_dataframe["categorized_as_feature_by_human"]).astype(float)
   return output_targets
 
 def my_input_fn(features, targets, batch_size=1, shuffle=True, num_epochs=None):
@@ -151,113 +144,109 @@ def construct_feature_columns(input_features):
   return set([tf.feature_column.numeric_column(my_feature)
               for my_feature in input_features])
 
-def train_nn_regression_model(
-    my_optimizer,
+def train_linear_classifier_model(
+    learning_rate,
     steps,
     batch_size,
-    hidden_units,
     training_examples,
     training_targets,
     validation_examples,
     validation_targets):
-  """Trains a neural network regression model.
+  """Trains a linear classification model.
   
   In addition to training, this function also prints training progress information,
   as well as a plot of the training and validation loss over time.
   
   Args:
-    my_optimizer: An instance of `tf.train.Optimizer`, the optimizer to use.
+    learning_rate: A `float`, the learning rate.
     steps: A non-zero `int`, the total number of training steps. A training step
       consists of a forward and backward pass using a single batch.
     batch_size: A non-zero `int`, the batch size.
-    hidden_units: A `list` of int values, specifying the number of neurons in each layer.
     training_examples: A `DataFrame` containing one or more columns from
-      `california_housing_dataframe` to use as input features for training.
+      `pull_requests_dataframe` to use as input features for training.
     training_targets: A `DataFrame` containing exactly one column from
-      `california_housing_dataframe` to use as target for training.
+      `pull_requests_dataframe` to use as target for training.
     validation_examples: A `DataFrame` containing one or more columns from
-      `california_housing_dataframe` to use as input features for validation.
+      `pull_requests_dataframe` to use as input features for validation.
     validation_targets: A `DataFrame` containing exactly one column from
-      `california_housing_dataframe` to use as target for validation.
+      `pull_requests_dataframe` to use as target for validation.
       
   Returns:
-    A `DNNRegressor` object trained on the training data.
+    A `LinearClassifier` object trained on the training data.
   """
 
   periods = 10
   steps_per_period = steps / periods
   
-  # Create a DNNRegressor object.
+  # Create a linear classifier object.
+  my_optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
   my_optimizer = tf.contrib.estimator.clip_gradients_by_norm(my_optimizer, 5.0)
-  dnn_regressor = tf.estimator.DNNRegressor(
+  linear_classifier = tf.estimator.LinearClassifier(
       feature_columns=construct_feature_columns(training_examples),
-      hidden_units=hidden_units,
-      optimizer=my_optimizer,
+      optimizer=my_optimizer
   )
   
   # Create input functions.
-  training_input_fn = lambda: my_input_fn(
-      training_examples, 
-      training_targets["cycle_time_in_minutes"], 
-      batch_size=batch_size)
-  predict_training_input_fn = lambda: my_input_fn(
-      training_examples, 
-      training_targets["cycle_time_in_minutes"], 
-      num_epochs=1, 
-      shuffle=False)
-  predict_validation_input_fn = lambda: my_input_fn(
-      validation_examples, validation_targets["cycle_time_in_minutes"], 
-      num_epochs=1, 
-      shuffle=False)
+  training_input_fn = lambda: my_input_fn(training_examples,
+                                          training_targets["categorized_as_feature_by_human"],
+                                          batch_size=batch_size)
+  predict_training_input_fn = lambda: my_input_fn(training_examples,
+                                                  training_targets["categorized_as_feature_by_human"],
+                                                  num_epochs=1,
+                                                  shuffle=False)
+  predict_validation_input_fn = lambda: my_input_fn(validation_examples,
+                                                    validation_targets["categorized_as_feature_by_human"],
+                                                    num_epochs=1,
+                                                    shuffle=False)
 
   # Train the model, but do so inside a loop so that we can periodically assess
   # loss metrics.
   print("Training model...")
-  print("RMSE (on training data):")
-  training_rmse = []
-  validation_rmse = []
+  print("LogLoss (on training data):")
+  training_log_losses = []
+  validation_log_losses = []
   for period in range (0, periods):
     # Train the model, starting from the prior state.
-    dnn_regressor.train(
+    linear_classifier.train(
         input_fn=training_input_fn,
         steps=steps_per_period
     )
     # Take a break and compute predictions.
-    training_predictions = dnn_regressor.predict(input_fn=predict_training_input_fn)
-    training_predictions = np.array([item['predictions'][0] for item in training_predictions])
+    training_probabilities = linear_classifier.predict(input_fn=predict_training_input_fn)
+    training_probabilities = np.array([item['probabilities'] for item in training_probabilities])
     
-    validation_predictions = dnn_regressor.predict(input_fn=predict_validation_input_fn)
-    validation_predictions = np.array([item['predictions'][0] for item in validation_predictions])
+    validation_probabilities = linear_classifier.predict(input_fn=predict_validation_input_fn)
+    validation_probabilities = np.array([item['probabilities'] for item in validation_probabilities])
     
-    # Compute training and validation loss.
-    training_root_mean_squared_error = math.sqrt(
-        metrics.mean_squared_error(training_predictions, training_targets))
-    validation_root_mean_squared_error = math.sqrt(
-        metrics.mean_squared_error(validation_predictions, validation_targets))
+    training_log_loss = metrics.log_loss(training_targets, training_probabilities)
+    validation_log_loss = metrics.log_loss(validation_targets, validation_probabilities)
     # Occasionally print the current loss.
-    print("  period %02d : %0.2f" % (period, training_root_mean_squared_error))
+    print("  period %02d : %0.2f" % (period, training_log_loss), flush=True)
     # Add the loss metrics from this period to our list.
-    training_rmse.append(training_root_mean_squared_error)
-    validation_rmse.append(validation_root_mean_squared_error)
+    training_log_losses.append(training_log_loss)
+    validation_log_losses.append(validation_log_loss)
   print("Model training finished.")
 
-  # Create a table with calibration data.
-  calibration_data = pd.DataFrame()
-  calibration_data["predictions"] = pd.Series(training_predictions)
-  calibration_data["targets"] = pd.Series(training_targets["cycle_time_in_minutes"])
-  #  print(calibration_data.describe())
-
-  # Plot predicted vs. actual
-  plt.title("Predictions vs. Actual")
-  plt.xlabel("predicted_cycle_time_in_minutes")
-  plt.ylabel("actual_cycle_time_in_minutes")
-  plt.scatter(calibration_data["predictions"], calibration_data["targets"])
+  # Output a graph of loss metrics over periods.
+  plt.ylabel("LogLoss")
+  plt.xlabel("Periods")
+  plt.title("LogLoss vs. Periods")
+  plt.tight_layout()
+  plt.plot(training_log_losses, label="training")
+  plt.plot(validation_log_losses, label="validation")
+  plt.legend()
   plt.savefig("/tmp/fig.png")
 
   # Output model bias + weights
-  #  print([(v, dnn_regressor.get_variable_value(v)) for v in dnn_regressor.get_variable_names()])
+  print([(v, linear_classifier.get_variable_value(v)) for v in linear_classifier.get_variable_names()])
 
-  return dnn_regressor
+  # Print evaluation metrics
+  evaluation_metrics = linear_classifier.evaluate(input_fn=predict_validation_input_fn)
+
+  print("AUC on the validation set: %0.2f" % evaluation_metrics['auc'])
+  print("Accuracy on the validation set: %0.2f" % evaluation_metrics['accuracy'])
+
+  return linear_classifier
 
 """
 End helper fns
@@ -266,23 +255,20 @@ End helper fns
 normalized_pull_requests_dataframe = normalize(preprocess_features(pull_requests_dataframe))
 
 # Set up training + validation examples
+training_examples = preprocess_features(pull_requests_dataframe.head(2000))
 normalized_training_examples = preprocess_features(normalized_pull_requests_dataframe.head(2000))
-print(normalized_training_examples.describe())
 
 training_targets = preprocess_targets(pull_requests_dataframe.head(2000))
-print(training_targets.describe())
 
+validation_examples = preprocess_features(pull_requests_dataframe.tail(500))
 normalized_validation_examples = preprocess_features(normalized_pull_requests_dataframe.tail(500))
-print(normalized_validation_examples.describe())
 
 validation_targets = preprocess_targets(pull_requests_dataframe.tail(500))
-print(validation_targets.describe())
 
-dnn_regressor = train_nn_regression_model(
-    my_optimizer=tf.train.GradientDescentOptimizer(learning_rate=0.01),
-    steps=5000,
+linear_classifier = train_linear_classifier_model(
+    learning_rate = 0.0001,
+    steps=10000,
     batch_size=200,
-    hidden_units=[6, 6],
     training_examples=normalized_training_examples,
     training_targets=training_targets,
     validation_examples=normalized_validation_examples,
